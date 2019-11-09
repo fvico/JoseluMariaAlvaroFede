8bcb3fdda836b9f27d952bcaf710e60f80857a3a
joselu <JosBY64@gmail.com>
2019-11-09 15:13:13 +0100

    script camara
    1

---
+++ Assets/Scripts/CameraPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour
+{
+    public GameObject target;
+
+
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+
+        transform.position = target.transform.position + offset;
+        transform.LookAt(target.transform.position);
+
+    }
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour{
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPoss;
+    public float attackRange;
+
+    void Update(){ 
+
+        if(timeBtwAttack <= 0){
+
+            if (Input.GetKey(KeyCode.J)) {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, );
+
+            }
+
+            timeBtwAttack = startTimeBtwAttack;
+        } else
+
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+
+    }
+
+
+}
