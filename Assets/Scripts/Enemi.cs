e53e5d3f4c7a507b94e942dce2eb7c4f422de3e1
Fede Vico <ifva94@gmail.com>
2019-11-09 13:00:58 +0100

    EnemiScript

---
+++ Assets/Scripts/Enemi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemi : MonoBehaviour
+{
+    public Rigidbody2D rigidbodyEnemi;
+    [Range(1, 10)] public float speedEnemi;
+    private void Start()
+    {
+        rigidbodyEnemi = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        rigidbodyEnemi.velocity = new Vector2(speedEnemi, 0f);
+    }
+}
---
+++ Assets/Scripts/LimitEnemi.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitEnemi : MonoBehaviour
+{
+    public GameObject enemi;
+    private Enemi currentSpeedEnemi;
+    private void Start()
+    {
+        currentSpeedEnemi = enemi.GetComponent<Enemi>();
+    }
+
+    private void Update()
+    {
+        if(currentSpeedEnemi.rigidbodyEnemi.velocity.x < 0)
+        {
+            currentSpeedEnemi.rigidbodyEnemi.velocity = new Vector2(-currentSpeedEnemi.rigidbodyEnemi.velocity.x,0f);
+        }
+    }
+}
