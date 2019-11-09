0ac0fcf7ced40949b57dd9a4e491a7f09cbd20d0
Fede Vico <ifva94@gmail.com>
2019-11-09 14:41:30 +0100

    ScriptEnemi
    Limit direcction Enemi and Load Scene when collision whit player

--- Assets/Scripts/Enemi.cs
+++ Assets/Scripts/Enemi.cs
@@ -2,16 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Enemi : MonoBehaviour
 {
     public Rigidbody2D rigidbodyEnemi;
     [Range(1, 10)] public float speedEnemi;
+    bool positiveDirection = false;
+    Vector2 currentSpeed;
     private void Start()
     {
         rigidbodyEnemi = GetComponent<Rigidbody2D>();
     }
     private void FixedUpdate()
     {
-        rigidbodyEnemi.velocity = new Vector2(speedEnemi, 0f);
+        currentSpeed = rigidbodyEnemi.velocity;
+        
+        if (positiveDirection)
+        {
+            rigidbodyEnemi.velocity = new Vector2(speedEnemi, 0f);
+        }
+        else
+        {
+            rigidbodyEnemi.velocity = new Vector2(-speedEnemi, 0f);
+        }
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "LimitEnemi")
+        {
+            if (currentSpeed.x < 0)
+            {
+                positiveDirection = true;
+            }
+            else
+            {
+                positiveDirection = false;
+            }
+        }
     }
 }
--- Assets/Scripts/LimitEnemi.cs
+++ Assets/Scripts/LimitEnemi.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LimitEnemi : MonoBehaviour
 {
     private Vector2 currentSpeedEnemi;
+    
 
     private void Update()
     {
