a7946a4ecc0b4705699f7f11cabe424cbc14e893
Fede Vico <ifva94@gmail.com>
2019-11-09 17:01:13 +0100

    CreatePickerAndWall

---
+++ Assets/Scripts/Picker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Picker : MonoBehaviour
+{
+    [Range(1,4)]public int numbObject;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
